a8e25361cc5390f2a2388d92b4e91ae6ee6da37b
Your Name <you@example.com>
2025-03-03 13:25:14 +0800

    Added event listener for submit and delete button for each book

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function displayBooks() {
   const bookList = document.getElementById("book-list");
   bookList.innerHTML = ""; // Clear table before adding new rows
 
-  myLibrary.forEach((book) => {
+  myLibrary.forEach((book, index) => {
     const row = document.createElement("tr");
 
     row.innerHTML = `
@@ -28,10 +28,22 @@ function displayBooks() {
           <td>${book.author}</td>
           <td>${book.numOfPages}</td>
           <td>${book.hasBeenRead ? "✔" : "❌"}</td>
-      `;
+          <td><button class="deleteBtn" data-index="${index}">Delete</button></td>
+          `;
 
     bookList.appendChild(row);
   });
+
+  const deleteButton = document.querySelectorAll(".deleteBtn");
+  deleteButton.forEach((button) => {
+    button.addEventListener("click", deleteBook);
+  });
+}
+
+function deleteBook(event) {
+  const index = event.target.getAttribute("data-index");
+  myLibrary.splice(index, 1);
+  displayBooks();
 }
 
 displayBooks();
@@ -43,3 +55,8 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Click has happened");
+});
